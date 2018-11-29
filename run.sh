c04414a0809bcb3690a2347476699c3c54850e9a
Nicolas Gif <nicolas.gif@free.fr>
2018-11-29 14:45:07 +0000

    Better authfile handling

--- run.sh
+++ run.sh
@@ -1,26 +1,42 @@
 #!/bin/bash
 
 MOUNTPOINT=/s3ql
+S3QL_HOME=/root/.s3ql
+AUTHFILE="$S3QL_HOME/authinfo2"
 
-if [ -n "$OS_PROJECT" ]
+# Create Authfile
+if [ ! -f "$AUTHFILE" ]
 then
-  export URL="swiftks://$OS_URL:$OS_CONTAINER"
-  sed -i "s#\$PASSWORD#$OS_PASSWORD#;
-          s#\$USER#$OS_USER#;
-          s#\$URL#$URL#;
-          s#\$PROJECT#$OS_PROJECT#;
-          s#\$CONTAINER#$OS_CONTAINER#" /root/.s3ql/authinfo2
-else
-  echo "No case foud" >&2
-  exit 1
+  if [ -z "$S3QL_PROJECT" ] || [ -z "$S3QL_USERNAME" ] || [ -z "$S3QL_PASSWORD" ] || [ -z "$S3QL_URL" ]
+  then
+    echo "Missing parameters" >&2
+    exit 1
+  fi
+
+  if [ ! -d "$S3QL_HOME" ]
+  then
+    mkdir -p "$S3QL_HOME"
+  fi
+
+  {
+    echo "[s3ql]";
+    echo "backend-login: $S3QL_PROJECT:$S3QL_USERNAME";
+    echo "backend-password: $S3QL_PASSWORD";
+    echo "storage-url: $S3QL_URL"
+  } > "$AUTHFILE"
+
+  if [ -n "$FS_PASSPHRASE" ]
+  then
+    echo "fs-passphrase: $FS_PASSPHRASE" >> "$AUTHFILE"
+  fi
+
+  chmod 600 "$AUTHFILE"
 fi
 
 # Create mountpoint if not exists
-[ ! -d "$MOUNTPOINT" ] && makdir -p "$MOUNTPOINT"
+[ ! -d "$MOUNTPOINT" ] && mkdir -p "$MOUNTPOINT"
 
-if [ -n "$FS_PASSWORD" ]
+if [ -f "$AUTHFILE" ]
 then
-  mount.s3ql --fg --log none --backend-options tcp-timeout=30 "$URL" "$MOUNTPOINT" <<< "$FS_PASSWORD"
-else
-  mount.s3ql --fg --log none --backend-options tcp-timeout=30 "$URL" "$MOUNTPOINT"
+  mount.s3ql --fg --log none --backend-options tcp-timeout=30 "$S3QL_URL" "$MOUNTPOINT"
 fi
