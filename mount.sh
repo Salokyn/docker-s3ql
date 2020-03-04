6d78a95f7936d5c4640ac54fd778917125d13a40
Nicolas Gif <ngf18490+gitlab@pm.me>
2020-03-04 21:47:43 +0000

    Add S3QL_AUTHFILE environment variable to specify a path to authinfo2 file

--- mount.sh
+++ mount.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-S3QL_HOME=/root/.s3ql
-AUTHFILE="$S3QL_HOME/authinfo2"
 PID=0
 
 term() {
@@ -26,10 +24,17 @@ then
 fi
 
 # Mount FS
-if [ -f "$AUTHFILE" ]
+if [ -f "$S3QL_AUTHFILE" ]
 then
+
+  # Set S3QL_URL if empty with first storage-url of authfile
+  if [ -z "$S3QL_URL" ]
+  then
+    S3QL_URL=$(sed -n 's/storage-url *: *\(.*\)/\1/p' "$S3QL_AUTHFILE"|head -n1)
+  fi
+
   # shellcheck disable=SC2086
-  fsck.s3ql $S3QL_FSCK_OPTIONS --batch "$S3QL_URL" && FSCK_RESULT=$?
+  fsck.s3ql $S3QL_FSCK_OPTIONS --authfile "$S3QL_AUTHFILE" --batch "$S3QL_URL" && FSCK_RESULT=$?
   if [ $FSCK_RESULT -ne 0 ] && [ $FSCK_RESULT -ne 128 ]; then
     echo "fsck.s3ql reported errors! Exit code $FSCK_RESULT - see http://www.rath.org/s3ql-docs/man/fsck.html"
     error
@@ -38,10 +43,10 @@ then
   trap 'term' TERM INT HUP
 
   # shellcheck disable=SC2086
-  mount.s3ql $S3QL_MOUNT_OPTIONS --fg "$S3QL_URL" "$MOUNTPOINT" & PID=$!
+  mount.s3ql $S3QL_MOUNT_OPTIONS --authfile "$S3QL_AUTHFILE" --fg "$S3QL_URL" "$MOUNTPOINT" & PID=$!
 
   wait $PID
 else
-  echo "Authfile not found"  >&2
+  echo "Authfile not found" >&2
   error
 fi
